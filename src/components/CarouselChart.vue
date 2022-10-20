dc48dc29ce15e706da0d965900d9e3992a1115e3
ciqiufuxue <944216048@qq.com>
2022-10-20 17:31:55 +0800

    图床链接修改为本地图片

--- src/components/CarouselChart.vue
+++ src/components/CarouselChart.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="carousel">
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" height="140" :lazy-render="true">
-      <van-swipe-item><img src="https://pic1.imgdb.cn/item/634e72bc16f2c2beb1a236b6.png" alt=""></van-swipe-item>
-      <van-swipe-item><img src="https://pic1.imgdb.cn/item/634e72ce16f2c2beb1a283a5.png" alt=""></van-swipe-item>
-      <van-swipe-item><img src="https://pic1.imgdb.cn/item/634e72d916f2c2beb1a2a75a.png" alt=""></van-swipe-item>
+      <van-swipe-item><img src="../assets/images/1.webp" alt=""></van-swipe-item>
+      <van-swipe-item><img src="../assets/images/2.webp" alt=""></van-swipe-item>
+      <van-swipe-item><img src="../assets/images/3.webp" alt=""></van-swipe-item>
     </van-swipe>
   </div>
 </template>
